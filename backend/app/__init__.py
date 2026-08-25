36a0f4fbf39e9270776b4981a16cbb73a8ade032
axiyea-jpg <axiyea@gmail.com>
2026-08-25 17:47:18 +0800

    Remove shell error prefix from backend/app/__init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,1 @@
-InvalidOperation: 
-Line |
-   2 |  [Console]::OutputEncoding=[Text.Encoding]::UTF8; Get-Content -Literal ��
-     |  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-     | Cannot set property. Property setting is supported only on core types in this language mode.
-
 
