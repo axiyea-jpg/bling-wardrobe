26f9e335b0603ed3deba406aeaa9d9a14b133975
axiyea-jpg <axiyea@gmail.com>
2026-08-25 17:41:16 +0800

    Add backend/app/__init__.py

---
+++ backend/app/__init__.py
@@ -0,0 +1,7 @@
+InvalidOperation: 
+Line |
+   2 |  [Console]::OutputEncoding=[Text.Encoding]::UTF8; Get-Content -Literal ��
+     |  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     | Cannot set property. Property setting is supported only on core types in this language mode.
+
+
